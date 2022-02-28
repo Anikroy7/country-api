ea53bc05f0dd5e0bcc1320a3a88efd7dfdab29d1
Anikroy7 <roy645800@gmail.com>
2022-02-28 17:12:29 +0600

    load country function added

--- country.js
+++ country.js
@@ -1,1 +1,15 @@
-console.log('connected')
+
+const loadCountry = () => {
+    console.log('Function connected')
+    fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => displayCountry(data))
+}
+
+const displayCountry = countries => {
+
+    for (const country of countries) {
+        console.log(country);
+
+    }
+}
