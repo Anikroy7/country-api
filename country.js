2172dd35ec323b37a6451e1d1b94ec4209558e7e
Anikroy7 <roy645800@gmail.com>
2022-02-28 18:56:21 +0600

    get search value added

--- country.js
+++ country.js
@@ -22,3 +22,27 @@ const displayCountry = countries => {
         displayId.appendChild(div)
     }
 }
+
+const loadCountryBySearchName = () => {
+    const searchId = document.getElementById('search-feild');
+    const searchText = searchId.value;
+    const url = `https://restcountries.com/v3.1/name/${searchText}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displaySearchCountry(data[0]))
+
+}
+const displaySearchCountry = country => {
+    console.log(country)
+    const displayId = document.getElementById('display-area');
+    document.getElementById('display-area').textContent = '';
+    const div = document.createElement('div');
+    div.classList.add('display-country')
+    div.innerHTML = `              
+        <h2>${country.name.common}</h2>
+        <p>${country.capital}</p>
+        <p>${country.population}</p>
+        <button onclick="displayDetails()" type="button" class="btn btn-info">Show details</button>
+        `
+    displayId.appendChild(div)
+}
